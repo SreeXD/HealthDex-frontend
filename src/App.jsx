f4bc9db7922d6ba009be297fb32371f28e1b508a
SreeXD <sree.nagaraj8@gmail.com>
2022-11-05 21:08:13 +0530

    setup firebase

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,27 @@
-import firebaseConfig from "./config/firebase.config"
-import { getAuth, RecaptchaVerifier } from "firebase/auth"
-import { initializeApp } from "firebase/app"
-import AuthContext from "./contexts/AuthContext"
-import { useEffect } from "react"
+import { useState } from 'react'
+import firebaseConfig from './config/firebase.config'
+import { initializeApp } from 'firebase/app'
+import { getAuth, signInWithPopup, GoogleAuthProvider, onAuthStateChanged } from 'firebase/auth'
+import AuthContext from './contexts/AuthContext'
 
 export default function App() {
     const app = initializeApp(firebaseConfig)
     const auth = getAuth(app)
+    const provider = new GoogleAuthProvider()
+    const [user, setUser] = useState(null)
+
+    const loginWithGoogle = () => {
+        signInWithPopup(auth, provider)
+    }
+
+    onAuthStateChanged(auth, (user) => {
+        setUser(user)
+    })
 
     return (
-        <AuthContext.Provider value={{ app, auth }}>
-
+        <AuthContext.Provider value={{ user }}>
+            <button onClick={loginWithGoogle}>login using google</button>
+            <div>{user?.email}</div>
         </AuthContext.Provider>
     )
 }
