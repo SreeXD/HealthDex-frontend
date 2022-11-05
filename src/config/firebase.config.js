47f7be4dbe190baa0f515de8ec55d149e8dcb37b
SreeXD <sree.nagaraj8@gmail.com>
2022-11-05 19:38:17 +0530

    initial commit

---
+++ src/config/firebase.config.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyCGlrWMNBY4nQ_JRiDr-Z7M5UGP2xmvzYQ",
+    authDomain: "hash-92655.firebaseapp.com",
+    projectId: "hash-92655",
+    storageBucket: "hash-92655.appspot.com",
+    messagingSenderId: "919463319619",
+    appId: "1:919463319619:web:fac2a03ccb18849b79dcf6",
+    measurementId: "G-C3YGKNEWHS"
+}
